3fefc9a8297a891a02762127404ea90fee91129d
Marlon Veiga <marlon_veiga@hotmail.com>
2021-01-27 18:26:14 -0300

    Exercícios dia 3

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,18 @@
 import React, { useState } from 'react';
 import { useRouter } from 'next/router';
-import styled from 'styled-components';
 import db from '../db.json';
 import Widget from '../src/components/Widget';
 import QuizLogo from '../src/components/QuizLogo';
 import QuizBackground from '../src/components/QuizBackground';
 import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
-
-export const QuizContainer = styled.div`
-  width: 100%;
-  max-width: 350px;
-  padding-top: 45px;
-  margin: auto 10%;
-  @media screen and (max-width: 500px) {
-    margin: auto;
-    padding: 15px;
-  }
-`;
+import QuizContainer from '../src/components/QuizContainer';
+import Input from '../src/components/Input';
+import Button from '../src/components/Button';
 
 export default function Home() {
   const router = useRouter();
   const [name, setName] = useState('');
-
-  const infosDoEvento = function infosDoEvento(e) {
-    e.preventDefault();
-    router.push(`/quiz?name=${name}`);
-  };
-
-  const handleChange = function change(e) {
-    setName(e.target.value);
-  };
 
   return (
     <QuizBackground backgroundImage={db.bg}>
@@ -41,14 +23,26 @@ export default function Home() {
             <h1>{db.title}</h1>
           </Widget.Header>
           <Widget.Content>
-            <form onSubmit={infosDoEvento}>
-              <input onChange={handleChange} placeholder="Diz ai seu nome" />
-              <button type="submit" disabled={name.length === 0}>
-                Jogar
-              </button>
+            <p>{db.description}</p>
+            <form onSubmit={(infosDoEvento) => {
+              infosDoEvento.preventDefault();
+              router.push(`/quiz?name=${name}`);
+              console.log('Fazendo uma submissão por meio do react');
+            }}
+            >
+              <Input
+                name="nomeDoUsuario"
+                onChange={(infosDoEvento) => setName(infosDoEvento.target.value)}
+                placeholder="Diz ai seu nome"
+                value={name}
+              />
+              <Button type="submit" disabled={name.length === 0}>
+                {`Vamos Jogar ${name} ?`}
+              </Button>
             </form>
           </Widget.Content>
         </Widget>
+
         <Widget>
           <Widget.Content>
             <h1>Quizes da Galera</h1>
@@ -58,7 +52,7 @@ export default function Home() {
         </Widget>
         <Footer />
       </QuizContainer>
-      <GitHubCorner projectUrl="https://github.com/omariosouto" />
+      <GitHubCorner projectUrl="https://github.com/marlonveiga-santos/marqueezz" />
     </QuizBackground>
   );
 }
